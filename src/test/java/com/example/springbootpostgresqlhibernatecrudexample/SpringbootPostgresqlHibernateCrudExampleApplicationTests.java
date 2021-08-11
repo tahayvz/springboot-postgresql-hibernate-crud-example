656e1f6e670879b8c15c0845008bea48308f580d
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2021-08-11 10:07:50 +0300

    first commit

---
+++ src/test/java/com/example/springbootpostgresqlhibernatecrudexample/SpringbootPostgresqlHibernateCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpostgresqlhibernatecrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPostgresqlHibernateCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
